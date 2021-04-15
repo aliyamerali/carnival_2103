266835013b0127d4d135a4cec2be13d30bfebb38
Aliya Merali <merali4@gmail.com>
2021-04-15 09:49:38 -0600

    Add tests for attendee array and admit method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -17,9 +17,13 @@ RSpec.describe Carnival do
     it 'starts with an empty array of rides' do
       expect(jeffco_fair.rides).to eq([])
     end
+
+    it 'starts with an empty array of attendees' do
+      expect(jeffco_fair.attendees).to eq([])
+    end
   end
 
-  describe 'instance methods' do
+  describe 'ride management methods' do
     jeffco_fair = Carnival.new("Jefferson County Fair")
     ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
     bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
@@ -42,6 +46,31 @@ RSpec.describe Carnival do
 
       expect(jeffco_fair.recommend_rides(bob)).to eq([ferris_wheel, bumper_cars])
       expect(jeffco_fair.recommend_rides(sally)).to eq([scrambler])
+    end
+  end
+
+  describe 'admission and attendee management methods' do
+    jeffco_fair = Carnival.new("Jefferson County Fair")
+    ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+    bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+    scrambler = Ride.new({name: 'Scrambler', cost: 15})
+    jeffco_fair.add_ride(ferris_wheel)
+    jeffco_fair.add_ride(bumper_cars)
+    jeffco_fair.add_ride(scrambler)
+    bob = Attendee.new("Bob", 0)
+    bob.add_interest('Ferris Wheel')
+    bob.add_interest('Bumper Cars')
+    sally = Attendee.new('Sally', 20)
+    sally.add_interest('Bumper Cars')
+    johnny = Attendee.new("Johnny", 5)
+    johnny.add_interest('Bumper Cars')
+
+    it '#admit adds attendees to an attendee array' do
+      jeffco_fair.admit(bob)
+      jeffco_fair.admit(sally)
+      jeffco_fair.admit(johnny)
+
+      expect(jeffco_fair.attendees).to eq([bob, sally, johnny])
     end
   end
 
