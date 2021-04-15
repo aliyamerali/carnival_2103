372dfdf5caafee0d7e755dda313ed39aaebb3223
Aliya Merali <merali4@gmail.com>
2021-04-15 09:32:59 -0600

    Add tests for recommend_rides

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -19,18 +19,30 @@ RSpec.describe Carnival do
     end
   end
 
-  describe '#add_ride' do
+  describe 'instance methods' do
     jeffco_fair = Carnival.new("Jefferson County Fair")
     ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
     bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
     scrambler = Ride.new({name: 'Scrambler', cost: 15})
 
-    jeffco_fair.add_ride(ferris_wheel)
-    jeffco_fair.add_ride(bumper_cars)
-    jeffco_fair.add_ride(scrambler)
+    it '#add_ride adds rides to the @rides array' do
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
 
-    it 'adds rides to the @rides array' do
       expect(jeffco_fair.rides).to eq([ferris_wheel, bumper_cars, scrambler])
     end
+
+    it '#recommend_rides returns an array of rides matching the attendees interests' do
+      bob = Attendee.new('Bob', 20)
+      sally = Attendee.new('Sally', 20)
+      bob.add_interest('Ferris Wheel')
+      bob.add_interest('Bumper Cars')
+      sally.add_interest('Scrambler')
+
+      expect(jeffco_fair.recommend_rides(bob)).to eq([ferris_wheel, bumper_cars])
+      expect(jeffco_fair.recommend_rides(sally)).to eq([scrambler])
+    end
   end
+
 end
