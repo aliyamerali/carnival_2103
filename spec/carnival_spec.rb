fee643f2d10bed98b981f8d837097cfb6d0b5a40
Aliya Merali <merali4@gmail.com>
2021-04-15 09:25:06 -0600

    Add initialize tests for carnival

---
+++ spec/carnival_spec.rb
@@ -0,0 +1,21 @@
+require './lib/ride'
+require './lib/attendee'
+require './lib/carnival'
+
+RSpec.describe Carnival do
+  describe '#initialize' do
+    jeffco_fair = Carnival.new("Jefferson County Fair")
+
+    it 'exists' do
+      expect(jeffco_fair).to be_instance_of(Carnival)
+    end
+
+    it 'has readable attribute name' do
+      expect(jeffco_fair.name).to eq("Jefferson County Fair")
+    end
+
+    it 'starts with an empty array of rides' do
+      expect(jeffco_fair.rides).to eq([])
+    end
+  end
+end
